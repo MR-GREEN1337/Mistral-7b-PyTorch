316127b74fb1a5991afefcae4a2be9603da1377d
ISLAM HACHIMI <94931434+MR-GREEN1337@users.noreply.github.com>
2023-12-31 00:14:13 +0000

    Adding RotaryPositionalEncoding

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class ModelArgs:
     n_kv_heads: Optional[int] = None
     vocab_size: int = -1
     multiple_of: int = 256
-    ffn_dim_multiplier: int = Optional[float] = None
+    ffn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
 
     # Needed for the KV cache
@@ -59,6 +59,37 @@ class RMSNorm(nn.Module):
         x /= norm_x
 
         return x * self.gamma
+
+class RotaryPostionalEncoding(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.d_model = args.d_model
+
+    @staticmethod
+    def compute_theta_params(d_model: int, end: int, theta: float = 10000.):
+        assert d_model % 2 ==0, "can't use an odd dimension!"
+        
+        freqs = 1.0 / (theta ** (torch.arange(0, d_model, 2)[: (d_model // 2)].float() / d_model))
+        t = torch.arange(end, device=freqs.device)
+        # Calculate m * theta_i
+        freqs = torch.outer(t, freqs).float()  # type: ignore
+        freqs_cisp = torch.polar(torch.ones_like(freqs), freqs)  # we don't care about the scalars for now, we set them to 1
+        return freqs_cisp
+    
+    def apply_rotary_embeddings(x: torch.Tensor, freq_complex: torch.Tensor, device: str):
+        x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+        freq_complex = freq_complex.unsqueeze(0).unsqueeze(2)
+        x_rotated = x_complex * freq_complex
+        x_out = torch.view_as_real(x_rotated)
+        x_out = x_out.reshape(*x.shape)
+
+        return x_out.type_as(x).to(device)
+
+    def forward(self, x):
+        R = torch.zeros(2, self.d_model)
+        R += x*torch.cos(compute_theta_params(self.d_model, ))
+
+
 
 class GQA(nn.Module):
     def __init__(self, args: ModelArgs):
